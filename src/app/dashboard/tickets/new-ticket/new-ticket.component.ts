40012cf8e03110d052e21be29dd58e0e0908ed5e
utsav <ukaran11@gmail.com>
2025-01-01 21:23:26 +0530

    Utsav | Added another shared comp control

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [],
+  imports: [ControlComponent],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
-
-}
+export class NewTicketComponent {}
